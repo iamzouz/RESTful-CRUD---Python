2ce3ff9fa0d59e60d2bfaec383cd7581a76a8727
Joseph Nehme <joseph.nehme.95@gmail.com>
2020-06-24 13:30:24 +0300

    Second commit

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from users.views import GetPost, GetPutDelete
 
-# below are the urls of users, to see urls of the project please go cmacgmAssignment.utls (there i included below urls).
+# below are the urls of users, to see urls of the project please go cmacgmAssignment.utls (there i included below urls)
 urlpatterns = [
     path('', GetPost.as_view()),
     path('<int:pk>/', GetPutDelete.as_view()),
